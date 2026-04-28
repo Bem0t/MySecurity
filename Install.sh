cbdbf11b0f69d18371a2a439b6bcae3915571320
Bem0t <71875399+Bem0t@users.noreply.github.com>
2026-04-28 23:30:39 +1000

    Repair2

--- Install.sh
+++ Install.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
-echo -e "Выберите вариант установки:\n"
 
-echo -e "1) Установить\n"
-echo -e "2) Удалить\n"
-echo -e "3) Сделать бэкап\n"
+echo "Выберите вариант установки:"
+echo "1) Установить"
+echo "2) Удалить"
+echo "3) Сделать бэкап"
+
 read -p "Выберите вариант установки: " var
 
 case "$var" in
-	1) 
-		echo "Установка\n" 
-		bash <(wget -qO- https://raw.githubusercontent.com/Bem0t/MySecurity/refs/heads/main/x-ui-pro.sh) -install yes -panel 1 -ONLY_CF_IP_ALLOW no
-		;;
-	2) 
-		echo "Удаление\n" 
-		sudo su -c "bash <(wget -qO- x-ui-pro.sh x-ui-pro.sh https://raw.githubusercontent.com/Bem0t/MySecurity/refs/heads/main/x-ui-pro.sh) -Uninstall yes"
-		;;
-	3) 
-		echo "Бэкап\n" 
-		sudo su -c "bash <(wget -qO- https://raw.githubusercontent.com/Bem0t/MySecurity/refs/heads/main/backup.sh)"
-		;;
-	*) echo "Неверный вариант" ;;
+    1)
+        echo "Установка"
+        bash <(wget -qO- https://raw.githubusercontent.com/Bem0t/MySecurity/refs/heads/main/x-ui-pro.sh) -install yes -panel 1 -ONLY_CF_IP_ALLOW no
+        ;;
+    2)
+        echo "Удаление"
+        bash <(wget -qO- https://raw.githubusercontent.com/Bem0t/MySecurity/refs/heads/main/x-ui-pro.sh) -Uninstall yes
+        ;;
+    3)
+        echo "Бэкап"
+        bash <(wget -qO- https://raw.githubusercontent.com/Bem0t/MySecurity/refs/heads/main/backup.sh)
+        ;;
+    *)
+        echo "Неверный вариант"
+        ;;
 esac
